0732c2ba9dde33253822c3a947bd65e6febfa398
Aditya Yeggina <yegginakumar@fb.com>
2023-12-28 17:15:50 +0530

    client requirement

--- vehicleDetails/admin.py
+++ vehicleDetails/admin.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from django.contrib import admin
-from .models import VehicleDetail,WorkerDetail
+from .models import VehicleDetail
 from django.contrib.auth.models import Group
 from import_export.admin import ImportExportModelAdmin
 from liststyle import ListStyleAdminMixin
@@ -12,53 +12,22 @@ from rangefilter.filters import (
     DateRangeQuickSelectListFilterBuilder,
 )
 
-from django_admin_listfilter_dropdown.filters import (
-    DropdownFilter, ChoiceDropdownFilter, RelatedDropdownFilter
-)
-
 admin.site.unregister(Group)
 
 
 # # Register your models here.
-class WorkerDetailsAdmin(ImportExportModelAdmin, ListStyleAdminMixin, admin.ModelAdmin):
-    list_display = (
-        "Worker_Name",
-        "Aadhar_Number",
-        "Address",
-        "Joined_Date",
-    )
-    search_fields = ["Worker_Name"]
 
 class VehicleDetailsAdmin(ImportExportModelAdmin, ListStyleAdminMixin, admin.ModelAdmin):
     list_display = (
-        "Customer_Name",
+        "Date",
         "Vehicle_Number",
-        "Contact_Number",
-        "Vin_Number",
-        "Wash_In_Timings",
-        "Wash_Out_Timings",
-        "Delivery_Time",
-        "Worker",
-        "Remarks",
-        "Service_Cost"
+        "Model"
     )
-    search_fields = ["Customer_Name", "Vehicle_Number", "Vin_Number","Delivery_Time"]
-   
-    # list_filter = (
-    #     # for ordinary fields
-    #     ('a_charfield', DropdownFilter),
-    #     # for choice fields
-    #     ('a_choicefield', ChoiceDropdownFilter),
-    #     # for related fields
-    #     ('a_foreignkey_field', RelatedDropdownFilter),
-    # )
-    
+    search_fields = ["Date", "Vehicle_Number", "Model"]
+
     list_filter = (
-        ("Worker", RelatedDropdownFilter),
-        ("Wash_In_Timings", DateRangeFilterBuilder()),
-        ("Wash_Out_Timings", DateRangeFilterBuilder()),
-        ("Delivery_Time", DateRangeFilterBuilder()), 
+        ("Date", DateRangeFilterBuilder()),
     )
 
-admin.site.register(WorkerDetail, WorkerDetailsAdmin)
+
 admin.site.register(VehicleDetail, VehicleDetailsAdmin)
